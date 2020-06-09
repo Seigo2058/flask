d8782a1d318eb8c79f4a520306832c581959eaaf
Seigo2058 <seigo040508@gmail.com>
2020-06-09 20:31:16 +0900

    ルーティングに/testを追加しました

--- templates/app.py
+++ templates/app.py
@@ -1,6 +1,10 @@
 from flask import Flask , render_template
-
+#flaskのflask,render_templateを使用します宣言
 app = Flask(__name__)
+
+@app.route("/test")
+def index():
+    return render_template("test.html")
 
 if __name__ == "__main__":
     #サーバーを起動するよ

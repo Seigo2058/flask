41bbcc181df2d12eecf1c6dd30d99498b77ed23b
Seigo2058 <seigo040508@gmail.com>
2020-06-10 20:20:15 +0900

    dbに接続

--- app.py
+++ app.py
@@ -20,7 +20,34 @@ def hello(text):
 def notfound(code):
     return "404ペーーーーージ"
 
+#2日目データベースの接続
+@app.route("/dbtest")
+def dbtest():
+    #データベースに接続
+    conn = sqlite3.connect('flask.db')
+    
+    #どこのデータを抜くかコンソールを充てる。カーソル→目印
+    c = conn.cursor()
+    
+    #execute:実行する
+    c.execute("select name, address from users where id=1")
+    
+    #fitchome:フェッチ:実際に所得する
+    user_info=c.fetchone()
+    
+    #データベース接続終了
+    c.close()
+    
+    #user_infoの中身を確認
+    print(user_info)
+    return render_template("dbtest.html", user_info = user_info)
 
+
+
+
+
+
+#ここから上に記述する
 if __name__ == "__main__":
     #サーバーを起動するよ
     app.run(debug=True)
